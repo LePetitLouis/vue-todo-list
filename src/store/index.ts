0bcb0bf6e077c57049b9b8caa45f1f3b4b550975
LePetitLouis <44061639+LePetitLouis@users.noreply.github.com>
2021-08-06 22:34:36 +0200

    store done

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: [{
+      title: String,
+      description: String,
+      done: Boolean
+    }]
   },
+
   mutations: {
+    ADD_TODO: (state, todo) => {
+      state.todos.push(todo)
+    },
+
+    DELETE_TODO: (state, todo) => {
+      state.todos.filter(el => el !== todo)
+    }
   },
+
   actions: {
+    addTodo: (store, todo) => {
+      store.commit('ADD_TODO', todo)
+    },
+
+    deleteTodo: (store, todo) => {
+      store.commit('DELETE_TODO', todo)
+    }
   },
-  modules: {
+
+  getters: {
+    todos: state => state.todos,
+    todosRemaining: state => state.todos.filter(todo => !todo.done),
+    todosDone: state => state.todos.filter(todo => todo.done),
+    todosRemainingCount: (state, getters) => getters.todosRemaining(state).length(),
+    todosDoneCount: (state, getters) => getters.todosDone(state).length()
   }
 })
